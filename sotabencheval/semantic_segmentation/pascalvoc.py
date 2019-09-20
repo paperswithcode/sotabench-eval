526256bcf17b2b9bed0f9817b85fed8b0d62a3ab
RJT1990 <rj-taylor@live.co.uk>
2019-09-20 17:29:55 +0100

    use np.around

--- sotabencheval/semantic_segmentation/pascalvoc.py
+++ sotabencheval/semantic_segmentation/pascalvoc.py
@@ -283,8 +283,8 @@ class PASCALVOCEvaluator(object):
         if not self.first_batch_processed:
             acc_global, acc, iu = self.voc_evaluator.compute()
             self.batch_hash = calculate_batch_hash(np.append(
-                np.append(np.round(targets, 3), np.round(outputs, 3)),
-                np.round(np.array([acc_global, acc, iu]), 3)))
+                np.append(np.around(targets, 3), np.around(outputs, 3)),
+                np.around(np.array([acc_global, acc, iu]), 3)))
             self.first_batch_processed = True
 
     def get_results(self):
