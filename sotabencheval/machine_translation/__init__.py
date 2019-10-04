81f14a4b543fe453e9f181f52aa3a482cff00586
Marcin <marcin.kardas@gmail.com>
2019-10-04 05:12:07 +0200

    Add WMT14 en-fr

--- sotabencheval/machine_translation/__init__.py
+++ sotabencheval/machine_translation/__init__.py
@@ -1,5 +1,5 @@
-from sotabencheval.machine_translation.wmt import WMTEvaluator
+from sotabencheval.machine_translation.wmt import WMTEvaluator, WMTDataset
 from sotabencheval.machine_translation.metrics import TranslationMetrics
 from sotabencheval.machine_translation.languages import Language
 
-__all__ = ["WMTEvaluator", "TranslationMetrics", "Language"]
+__all__ = ["WMTDataset", "WMTEvaluator", "TranslationMetrics", "Language"]
--- sotabencheval/machine_translation/wmt.py
+++ sotabencheval/machine_translation/wmt.py
@@ -2,14 +2,26 @@ from sotabencheval.core import BaseEvaluator
 from sotabencheval.utils import calculate_batch_hash, change_root_if_server, is_server
 from sotabencheval.machine_translation.languages import Language
 from sotabencheval.machine_translation.metrics import TranslationMetrics
-from typing import Dict, List
+from typing import Dict
 from pathlib import Path
+from enum import Enum
+
+
+class WMTDataset(Enum):
+    News2014 = "newstest2014"
+    News2019 = "newstest2019"
 
 
 class WMTEvaluator(BaseEvaluator):
     task = "Machine Translation"
+    _datasets = {
+        (WMTDataset.News2014, Language.English, Language.German): "WMT2014 English German",
+        (WMTDataset.News2019, Language.English, Language.German): "WMT2019 English German",
+        (WMTDataset.News2014, Language.English, Language.French): "WMT2014 English French",
+    }
 
     def __init__(self,
+                 dataset: WMTDataset,
                  source_lang: Language,
                  target_lang: Language,
                  local_root: str = '.',
@@ -23,21 +35,33 @@ class WMTEvaluator(BaseEvaluator):
         super().__init__(model_name, paper_arxiv_id, paper_pwc_id, paper_results, model_description)
         self.root = change_root_if_server(root=local_root,
                                           server_root=".data/nlp/wmt")
+        self.dataset = dataset
         self.source_lang = source_lang
         self.target_lang = target_lang
 
+        default_src_fn, default_dst_fn = self._get_source_dataset_filename()
         if source_dataset_filename is None or is_server():
-            source_dataset_filename = "newstest2019-{0}{1}-src.{0}.sgm". \
-                format(self.source_lang.value, self.target_lang.value)
+            source_dataset_filename = default_src_fn
 
         if target_dataset_filename is None or is_server():
-            target_dataset_filename = "newstest2019-{0}{1}-ref.{1}.sgm". \
-                format(self.source_lang.value, self.target_lang.value)
+            target_dataset_filename = default_dst_fn
 
         self.source_dataset_path = Path(self.root) / source_dataset_filename
         self.target_dataset_path = Path(self.root) / target_dataset_filename
 
         self.metrics = TranslationMetrics(self.source_dataset_path, self.target_dataset_path)
+
+    def _get_source_dataset_filename(self):
+        if self.dataset == WMTDataset.News2014:
+            other_lang = self.source_lang.value if self.target_lang == Language.English else self.target_lang.value
+            source = "{0}-{1}en-src.{2}.sgm".format(self.dataset.value, other_lang, self.source_lang.value)
+            target = "{0}-{1}en-ref.{2}.sgm".format(self.dataset.value, other_lang, self.target_lang.value)
+        elif self.dataset == WMTDataset.News2019:
+            source = "{0}-{1}{2}-src.{1}.sgm".format(self.dataset.value, self.source_lang.value, self.target_lang.value)
+            target = "{0}-{1}{2}-ref.{2}.sgm".format(self.dataset.value, self.source_lang.value, self.target_lang.value)
+        else:
+            raise ValueError("Unknown dataset: {}".format(self.dataset))
+        return source, target
 
     def add(self, answers: Dict[str, str]):
         self.metrics.add(answers)
@@ -59,9 +83,14 @@ class WMTEvaluator(BaseEvaluator):
         return self.results
 
     def save(self):
-        datasets = {
-            ("2019", Language.English, Language.German): "WMT2019 English German"
-        }
-        dataset = datasets.get(("2019", Language.English, Language.German))
+        cfg = (self.dataset, self.source_lang, self.target_lang)
+        if cfg not in WMTEvaluator._datasets:
+            raise ValueError("Unsupported dataset configuration: {} {} {}".format(
+                self.dataset.name,
+                self.source_lang.name,
+                self.target_lang.name
+            ))
+
+        dataset = WMTEvaluator._datasets.get(cfg)
         return super().save(dataset=dataset)
 
