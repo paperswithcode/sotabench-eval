9034c66754478cc43d9117d16e4b407d3d082c16
Sam Hardwick <sam.hardwick@iki.fi>
2021-05-20 15:47:38 +0300

    Add Tatoeba modules

--- sotabencheval/machine_translation/__init__.py
+++ sotabencheval/machine_translation/__init__.py
@@ -1,5 +1,6 @@
 from sotabencheval.machine_translation.wmt import WMTEvaluator, WMTDataset
 from sotabencheval.machine_translation.metrics import TranslationMetrics
 from sotabencheval.machine_translation.languages import Language
+from sotabencheval.machine_translation.tatoeba import TatoebaEvaluator, TatoebaDataset
 
-__all__ = ["WMTDataset", "WMTEvaluator", "TranslationMetrics", "Language"]
+__all__ = ["WMTDataset", "WMTEvaluator", "TranslationMetrics", "Language", "TatoebaEvaluator", "TatoebaDataset"]
